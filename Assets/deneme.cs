2c14c4ec185236657ea4e2267e3474b0971b185f
berkayalty <73275982+berkayalty@users.noreply.github.com>
2021-11-21 23:51:42 +0300

    delete

--- Assets/deneme.cs
+++ Assets/deneme.cs
@@ -10,16 +10,11 @@ public class deneme : MonoBehaviour
 
     void Start()
     {
-        Debug.Log("berkay altay kod ustası");
         whiteBall = GameObject.FindWithTag("whiteBall");
         whiteBall.transform.localPosition = new Vector3(240f, 67.82f, 61.86f);
     }
 
     void Update()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
-        // whiteBall.transform.position = transform.position + new Vector3(horizontalInput * movementSpeed * Time.deltaTime, verticalInput * movementSpeed * Time.deltaTime, 0);
-        // Debug.Log(transform.position);
     }
 }
